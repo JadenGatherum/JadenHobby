965fb560fbcc865a41c49e612018fe2c27f5b248
JadenGatherum <jadenbb@byu.edu>
2024-02-09 18:26:35 -0700

    Add project files.

---
+++ JadenHobby/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿$("#btnCalc").click(function () {
+    var hoursTutored = parseFloat($("#hoursTutored").val());
+    var charge = 0;
+    var outputBox = $("#readonlyInput");
+
+    if (hoursTutored <= 0 || isNaN(hoursTutored)) {
+        // Makes sure the number is positive and not NaN
+        alert("That is not a valid number, please enter a positive number");
+        $("#hoursTutored").focus();
+    } else {
+        // calculate the total
+        charge = (($("#hourlyCharge").val()) * hoursTutored);
+        outputBox.val(charge);
+    }
+});
